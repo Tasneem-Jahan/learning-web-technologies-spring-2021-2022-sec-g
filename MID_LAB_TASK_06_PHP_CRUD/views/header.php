f5220823ca76dc48bc4928647b4ae880fc665745
Tasneem Jahan <areenfar@gmail.com>
2022-02-22 10:26:52 +0600

    Worked on CRUD operation.

---
+++ MID_LAB_TASK_06_PHP_CRUD/views/header.php
@@ -0,0 +1,5 @@
+<?php 
+	session_start();
+	if(!isset($_COOKIE['status'])){ 
+		header('location: login.php');
+	}
---
+++ MID_LAB_TASK_06_PHP_CRUD/views/home.php
@@ -0,0 +1,24 @@
+<?php
+//include("header.php");
+//include_once("header.php");
+
+require('header.php');
+//require_once('header.php');
+?>
+
+<html>
+
+<head>
+    <title>Home Page</title>
+</head>
+
+<body>
+    <h1>Welcome home!</h1>
+
+    <a href="create.php"> Create New User</a> |
+    <a href="show.php"> User List</a> |
+    <a href="../controllers/logout.php"> logout</a>
+
+</body>
+
+</html>
